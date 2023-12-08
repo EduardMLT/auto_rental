a62121ca7db5e124c23bf0c9e234276d4a407411
EduardMLT <eddku@i.ua>
2023-12-08 23:49:08 +0200

    initial

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+
 import {store, persistor}  from './redux/store';
-import { PersistGate } from "redux-persist/integration/react";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
